f8cda28187104f29206b4056c297d550702ef0bc
Choukrallah Lachhab <choukrallah_lachhab@um5.ac.ma>
2022-08-11 20:45:32 +0100

    User Routes, Controller & Model

---
+++ back_end/Controllers/userController.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const User= require('../Models/userModel')
+
+
+const loginUser = async(req,res) => {
+    
+}
+
+const signupUser = async(req,res) => {
+
+    const {email,password} = req.body
+    
+    try {
+        const user = await User.signup(email,password)
+        res.status(200).json({email,user})
+    } catch(error){
+        res.status(400).json({error : error.message})
+    }
+}
+
+module.exports = {
+    loginUser,
+    signupUser
+}
--- back_end/server.js
+++ back_end/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutsRoutes = require('./Routes/workoutsRoutes')
+const userRoutes = require('./Routes/userRoutes')
 
 const app = express()
 app.use(express.json())
@@ -10,6 +11,7 @@ app.use(express.json())
 
 app.use('/api/workouts',workoutsRoutes)
 
+app.use('/api/user',userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
    .then(()=>{
